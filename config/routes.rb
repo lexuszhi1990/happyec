8ba2320dcc8dcfddbaa2be7e509304e0b0dbeb47
Peter Wang <happypeter1983@gmail.com>
2011-12-07 10:23:55 +0800

    try to add users by hand

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,7 @@ Ec::Application.routes.draw do
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    resources :users
 
   # Sample resource route with options:
   #   resources :products do
