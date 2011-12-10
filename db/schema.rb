2e67e48db54b2402b6fda921c12d9018be7f2974
Peter Wang <happypeter1983@gmail.com>
2011-12-10 17:17:27 +0800

    r g migration AddAncestryToComments ancestry:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111207132621) do
+ActiveRecord::Schema.define(:version => 20111210091703) do
 
   create_table "comments", :force => true do |t|
     t.integer  "event_id"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20111207132621) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "ancestry"
   end
 
   create_table "events", :force => true do |t|
