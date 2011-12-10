5bb459264a1335c8b4aec42347fcf35afb4b62cb
Peter Wang <happypeter1983@gmail.com>
2011-12-10 21:03:21 +0800

    ancestry for reply

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,7 +24,7 @@ class CommentsController < ApplicationController
   # GET /comments/new
   # GET /comments/new.xml
   def new
-    @comment = Comment.new(:post_id => params[:post_id])
+    @comment = Comment.new(:event_id => params[:event_id], :user_id => params[:user_id], :parent_id => params[:parent_id])
 
     respond_to do |format|
       format.html # new.html.erb
