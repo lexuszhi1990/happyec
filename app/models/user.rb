b74d6f44b3433d379c27b294e81990fa446b0033
Peter Wang <happypeter1983@gmail.com>
2012-01-12 06:06:16 -0500

    this cause problem when login-with-qq

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ActiveRecord::Base
   before_create { generate_token(:token) }
 
   validates_confirmation_of :password  
-  validates_presence_of :password, :on => :create
 
   validates_presence_of :name  
   validates_uniqueness_of :name  
