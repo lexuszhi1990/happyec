d749ce569f7dda296f945053b9200a98bff21d70
Peter Wang <happypeter1983@gmail.com>
2012-01-12 06:23:49 -0500

    the name from qq shows up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
+    @current_user ||= User.find_by_token(cookies[:token]) if cookies[:token]
+  end
 end
