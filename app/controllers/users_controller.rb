35dc3b27dcdf870c49ea73871da91155787e9b95
Peter Wang <happypeter1983@gmail.com>
2012-03-07 19:57:26 +0800

    typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -95,7 +95,7 @@ class UsersController < ApplicationController
   def create  
     @user = User.new(params[:user])  
     if @user.save  
-      PeterMailer.registration_confirmation(u).deliver
+      PeterMailer.registration_confirmation(@user).deliver
       session[:user_id] = @user.id 
       redirect_to root_url, :notice => "signed up!"  
     else  
