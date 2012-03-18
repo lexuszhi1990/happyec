cad294fc7770f512889359d6a3b5d00cca8e2002
Peter Wang <happypeter1983@gmail.com>
2012-03-18 23:23:19 +0800

    I really want to display <>, and I also want hard_wrap linebreaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def escape_arrow(text)
+    text.gsub("<",'&lt;').gsub(">",'&gt;')
+  end
+  def line_break(text)
+    text.gsub("\n",'<br \>')
+  end
   def markdown(text)
     options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis]
     Redcarpet.new(text, *options).to_html.html_safe
