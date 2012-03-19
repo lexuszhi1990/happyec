f59840708972a2f37bea30016a976ef964463f87
Peter Wang <happypeter1983@gmail.com>
2012-03-19 09:51:09 +0800

    simple_format is more powerful than my escape_arrow&line_break
    and it is html safe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,6 @@
 module ApplicationHelper
-  def escape_arrow(text)
-    text.gsub("<",'&lt;').gsub(">",'&gt;')
-  end
-  def line_break(text)
-    text.gsub("\n",'<br \>')
-  end
   def markdown(text)
     options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis]
     Redcarpet.new(text, *options).to_html.html_safe
-    
   end
 end
