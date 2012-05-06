e783fca2abf1127ef08effbbd5c1fa2c5a93a1db
Peter Wang <happypeter1983@gmail.com>
2012-05-07 07:25:24 +0800

    default_url for gravatar works

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
 
   def avatar_url(user)
+    default_url = "#{root_url}assets/meetup_head.jpg"
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
   end
 
   def markdown(text)
