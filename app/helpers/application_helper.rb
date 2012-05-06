759daa4034623845eebd9c2f88f95fc88d91516a
Peter Wang <happypeter1983@gmail.com>
2012-05-07 07:00:38 +0800

    the avatar shows up, now using gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png"
+  end
+
   def markdown(text)
     options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis]
     Redcarpet.new(text, *options).to_html.html_safe
