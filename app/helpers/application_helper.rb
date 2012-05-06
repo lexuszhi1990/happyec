5509c3f70a25252752d999894fec80e8b3aa8b08
Peter Wang <happypeter1983@gmail.com>
2012-05-07 07:48:06 +0800

    change default_url, #5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def avatar_url(user)
-    default_url = "#{root_url}assets/meetup_head.jpg"
+    default_url = "#{root_url}assets/cat.png"
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
   end
