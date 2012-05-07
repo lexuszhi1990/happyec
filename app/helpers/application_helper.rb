8f077347b6e85c8057fe01e6d830287148f74fa2
Peter Wang <happypeter1983@gmail.com>
2012-05-07 10:49:22 +0800

    bring gravatar back, qq is fucked, #5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 module ApplicationHelper
 
   def avatar_url(user)
-    user.image_url
+    default_url = "#{root_url}assets/cat.png"
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
   end
 
   def markdown(text)
